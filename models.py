a54f1ead292a9376194fe3d677bff8adb7d8e4bc
Alper Özkurt <alper98ozx@gmail.com>
2026-07-15 15:56:15 +0300

    user and wallet now uses orm and works on the database

--- models.py
+++ models.py
@@ -1,22 +1,80 @@
 from datetime import datetime
 from dataclasses import dataclass, field
+from database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime, Enum, Integer, String, Identity, ForeignKey
 import enums
     
-@dataclass
-class User:
-    user_id: int
-    username: str
-    status: enums.UserStatus = enums.UserStatus.ACTIVE
-    created_at: datetime = field(default_factory=datetime.now) 
+class User(Base):
+    __tablename__ = "users"
+    
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        Identity(start=1),
+        primary_key=True
+    )
+    
+    username: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False,
+        unique=True
+    )
+    
+    status: Mapped[enums.UserStatus] = mapped_column(
+        Enum(enums.UserStatus),
+        default=enums.UserStatus.ACTIVE,
+        nullable=False
+    )
+    
+    wallets: Mapped[list[Wallet]] = relationship(
+        back_populates="owner"
+    )
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        nullable=False
+    ) 
               
-@dataclass
-class Wallet:
-    wallet_id: int
-    owner: User 
-    currency: enums.Currency
-    balance: int = 0
-    status: enums.WalletStatus = enums.WalletStatus.ACTIVE
-    created_at: datetime = field(default_factory=datetime.now) 
+class Wallet(Base):
+    __tablename__ = "wallets"
+    
+    wallet_id: Mapped[int] = mapped_column(
+        Integer,
+        Identity(start=1),
+        primary_key=True
+    )
+    
+    owner_id: Mapped[int] = mapped_column(
+        ForeignKey("users.user_id"),
+        nullable=False
+    )
+    
+    owner: Mapped[User] = relationship(
+        back_populates="wallets"
+    ) 
+    
+    currency: Mapped[enums.Currency] = mapped_column(
+        Enum(enums.Currency),
+        nullable=False
+    )
+    
+    balance: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False
+    )
+    
+    status: Mapped[enums.WalletStatus] = mapped_column(
+        Enum(enums.WalletStatus),
+        default=enums.WalletStatus.ACTIVE,
+        nullable=False
+    )
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        nullable=False
+    )
     
 
 @dataclass     
